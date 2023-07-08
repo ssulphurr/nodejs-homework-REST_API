4c65eb9ca6bdfbd3d68c8a8eedc95162392a9d79
ssulphurr <ssulphurr@gmail.com>
2023-07-08 21:36:29 +0300

    adds validation with Joi

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const Joi = require("joi");
 
 const router = express.Router();
 
 const contacts = require("../../models/contacts");
 
 const { HttpError } = require("../../helpers/HttpError");
+
+const joiSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
 router.get("/", async (req, res, next) => {
   try {
@@ -29,11 +36,13 @@ router.get("/:contactId", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const result = await contacts.addContact(req.body);
-    if (!result) {
-      throw HttpError(404, "Not Found");
+    const { error } = joiSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
     }
-    res.json(result);
+
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
   } catch (error) {
     next(error);
   }
@@ -53,11 +62,12 @@ router.delete("/:contactId", async (req, res, next) => {
 
 router.put("/:contactId", async (req, res, next) => {
   try {
-    const result = await contacts.updateContact(req.params.contactId, req.body);
-    console.log(result);
-    if (!result) {
-      throw HttpError(404, "Bad request");
+    const { error } = joiSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
     }
+
+    const result = await contacts.updateContact(req.params.contactId, req.body);
     res.json(result);
   } catch (error) {
     next(error);
