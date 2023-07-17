a0dbbf66ed93f7d1973532b3b757e0af8c942c6e
ssulphurr <ssulphurr@gmail.com>
2023-07-17 20:36:36 +0300

    adds throwing error in ctrl instestead of next

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -7,40 +7,41 @@ const getAll = async (req, res) => {
   res.json(result);
 };
 
-const getById = async (req, res, next) => {
+const getById = async (req, res) => {
   const result = await contacts.getContactById(req.params.contactId);
   if (!result) {
-    return next(new HttpError(404));
+    throw new HttpError(404);
   }
   res.json(result);
 };
 
-const addContact = async (req, res, next) => {
+const addContact = async (req, res) => {
   const { error } = joiSchema.validate(req.body);
   if (error) {
-    return next(new HttpError(400, error.message));
+    throw new HttpError(400, error.message);
   }
 
   const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
 
-const deletById = async (req, res, next) => {
+const deletById = async (req, res) => {
   const result = await contacts.removeContact(req.params.contactId);
   if (!result) {
-    return next(new HttpError(404));
+    throw new HttpError(404);
   }
   res.json({ message: "Contact deleted" });
 };
 
-const updateById = async (req, res, next) => {
-  const result = await contacts.updateContact(req.params.contactId, req.body);
-  if (!result) {
-    return next(new HttpError(404));
-  }
+const updateById = async (req, res) => {
   const { error } = joiSchema.validate(req.body);
   if (error) {
-    return next(new HttpError(400, error.message));
+    throw new HttpError(400, error.message);
+  }
+
+  const result = await contacts.updateContact(req.params.contactId, req.body);
+  if (!result) {
+    throw new HttpError(404);
   }
 
   res.json(result);
