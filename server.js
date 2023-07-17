f6aa4c569b58ac5a6621b0093f3da8d3862d2652
ssulphurr <ssulphurr@gmail.com>
2023-07-17 22:31:56 +0300

    connects app with database

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Sonia:NQMkc2k9E5M61K9U@cluster0.gqpmkzw.mongodb.net/Contact_Book?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connacted");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => console.log(error.message));
