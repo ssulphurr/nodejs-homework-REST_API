f17cebd2f32252fdac545caf9f316d650619b1e8
ssulphurr <ssulphurr@gmail.com>
2023-07-17 22:34:25 +0300

    adds error processing in server

--- server.js
+++ server.js
@@ -12,4 +12,7 @@ mongoose
       console.log("Server running. Use our API on port: 3000");
     });
   })
-  .catch((error) => console.log(error.message));
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
