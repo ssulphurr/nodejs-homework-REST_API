33d73444c339e2fbd9446737c48212d1ec1315f1
ssulphurr <ssulphurr@gmail.com>
2023-07-26 00:58:44 +0300

    adds bcrypt for password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,19 @@
+const bcrypt = require("bcrypt");
+
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw new HttpError(409, "Email in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashRassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashRassword });
 
   res.json({ email: newUser.email });
 };
