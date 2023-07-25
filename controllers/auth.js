e7dd77586f97fc5e7b1284f1da701c6bdf0da781
ssulphurr <ssulphurr@gmail.com>
2023-07-26 00:44:54 +0300

    adds message to MongoServerError from HttpError

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,13 @@ const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const register = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw new HttpError(409, "Email in use");
+  }
+
   const newUser = await User.create(req.body);
 
   res.json({ email: newUser.email });
