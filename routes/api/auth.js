9b54f4f88c8415f937bb323215e918ec397f1a47
ssulphurr <ssulphurr@gmail.com>
2023-07-26 02:38:21 +0300

    adds "/users" to routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,12 +8,16 @@ const ctrl = require("../../controllers/auth");
 
 const router = express.Router();
 
-router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post(
+  "/users/register",
+  validateBody(schemas.registerSchema),
+  ctrl.register
+);
 
-router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+router.post("/users/login", validateBody(schemas.loginSchema), ctrl.login);
 
-router.post("/current", authenticate, ctrl.getCurrent);
+router.post("/users/current", authenticate, ctrl.getCurrent);
 
-router.post("/logout", authenticate, ctrl.logout);
+router.post("/users/logout", authenticate, ctrl.logout);
 
 module.exports = router;
