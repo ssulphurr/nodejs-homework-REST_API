403c82378ed46ad75751cc2cbe3a9a517001bf38
ssulphurr <ssulphurr@gmail.com>
2023-08-01 14:22:41 +0300

    rewrites contacts joi schemas exports

--- helpers/index.js
+++ helpers/index.js
@@ -1,13 +1,17 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./handleMongooseError");
-const contactSchemas = require("./contactSchemas");
+const {
+  createContactValidationSchema,
+  updateFavoriteValidationSchema,
+} = require("./contactSchemas");
 const userSchemas = require("./userSchemas");
 
 module.exports = {
   HttpError,
   ctrlWrapper,
   handleMongooseError,
-  contactSchemas,
+  createContactValidationSchema,
+  updateFavoriteValidationSchema,
   userSchemas,
 };
